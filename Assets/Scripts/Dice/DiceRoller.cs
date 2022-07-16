732e40470f21e1687c7e80461d96f5b66524513c
kyle <kylengelmann@gmail.com>
2022-07-16 11:22:48 -0500

    Bad dice rolling

---
+++ Assets/Scripts/Dice/DiceRoller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoller : MonoBehaviour
+{
+    public GameObject DiePrefab;
+
+    public float RollHeight = 1f;
+
+    void Update()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.Log(mouseRay.origin);
+
+        Vector3 mouseLocation = mouseRay.origin + mouseRay.direction * (RollHeight - mouseRay.origin.y) / mouseRay.direction.y;
+
+        transform.position = mouseLocation;
+
+        if(Input.GetMouseButtonDown(0) && DiePrefab)
+        {
+
+            for (int i = 0; i < 4; ++i)
+            {
+                GameObject die = Instantiate<GameObject>(DiePrefab, transform.position + transform.lossyScale.z * Vector3.forward + Random.insideUnitSphere, Random.rotation);
+                die.GetComponent<Rigidbody>().AddForce(Vector3.forward * Random.Range(2f, 6f) + Random.insideUnitSphere * .5f, ForceMode.VelocityChange);
+                die.GetComponent<Rigidbody>().AddTorque(Random.onUnitSphere * Random.Range(3f, 12f), ForceMode.VelocityChange);
+            }
+        }
+    }
+}
